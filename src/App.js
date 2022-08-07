e996e5f2ad1a062f242e7feb79259b715558f9f8
Developer Junaid <qjunaid623@gmail.com>
2022-08-07 12:55:19 +0500

    added banner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import "./App.css";
+
+// React Router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/checkout" element={<h1>Checkout</h1>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <Header />
+              <h1>Login</h1>
+            </>
+          }
+        />
+        <Route
+          path="/checkout"
+          element={
+            <>
+              <Header />
+              <h1>Checkout</h1>
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Styles
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://m.media-amazon.com/images/I/717OO5QwJnL._SX3000_.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Home;
